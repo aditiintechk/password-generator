1d66d0193391948711dd870976c7089b3a2a124c
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-14 07:41:18 +0530

    Add function to generate password

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const generatePasswordBtn = document.getElementById("pwdButton");
 const passwordDisplayOne = document.getElementById("pwdOne");
 const passwordDisplayTwo = document.getElementById("pwdTwo");
 
+
+generatePasswordBtn.addEventListener("click", generatePassword);
+
+function generatePassword() {
+    let randomPasswordOne = "";
+    let randomPasswordTwo = "";
+    for(let i = 0; i < 15; i++) {
+        randomPasswordOne += getrandomCharacter();
+        randomPasswordTwo += getrandomCharacter();
+    }
+    passwordDisplayOne.textContent = randomPasswordOne;
+    passwordDisplayTwo.textContent = randomPasswordTwo;
+}
+
 function getrandomCharacter() {
     let randomChar = Math.floor(Math.random() * characters.length);
     return characters[randomChar];
